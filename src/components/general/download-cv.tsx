592e1d5a1dd3cb7d2a5162df94eb05f1b7d2c0d0
Partha Borah <143783492+laatu08@users.noreply.github.com>
2025-06-18 07:51:39 +0530

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/Partha Borah latest resume 2.pdf', '_blank')}>
+    <Button onClick={() => window?.open('/files/Partha Borah latest resume 3.0.pdf', '_blank')}>
       Download Resume
     </Button>
   );
